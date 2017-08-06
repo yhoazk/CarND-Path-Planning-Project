4e86d93ed192587d633173c5018d55a44d7116f2
porko <yhoazk@gmail.com>
2017-08-06 02:05:56 -0500

    Corrected states for input data

--- L3/naive_bayes/solution/classifier.cpp
+++ L3/naive_bayes/solution/classifier.cpp
@@ -32,7 +32,57 @@ void GNB::train(vector<vector<double>> data, vector<string> labels)
 
     labels - array of N labels
       - Each label is one of "left", "keep", or "right".
+
+      - Count the number of labels
+      - Count the number of labels for each type
+      - get the mean omega_d for each label
+      - get the variance sum (x- x_mean)^ 2
+
   */
+  size_t count_left  = 0;
+  size_t count_keep  = 0;
+  size_t count_right = 0;
+
+  double_t sum_left  = 0;
+  double_t sum_keep  = 0;
+  double_t sum_right = 0;
+
+  if(data.size() != labels.size())
+  {
+    /* Error, vectors do not match */
+    return;
+  } else {
+
+    for(size_t i=0; i < data.size(); ++i)
+    {
+      if(0 == labels[i].compare(possible_labels[0]))
+      {
+        /*Left*/
+        ++count_left;
+        sum_left += fmod(data[i][1] , lane_width) + data[i][1]/lane_width;
+      }
+      else if(0 == labels[i].compare(possible_labels[1]))
+      {
+        /*keep*/
+        ++count_keep;
+        sum_keep += fmod(data[i][1], lane_width) + data[i][1]/lane_width;
+
+      } else{
+        /* Assuming that there are not other values than the possible_labels */
+        /* right */
+        ++count_right;
+        sum_right += fmod(data[i][1], lane_width) + data[i][1]/lane_width;
+
+      }
+    }
+    std::cout << "=========================================" << std::endl;
+    std::cout << "Mean left: " << sum_left /* (double_t) count_left */ << std::endl;
+    std::cout << "Mean keep: " << sum_keep /*(double_t) count_keep*/  << std::endl;
+    std::cout << "Mean right: " <<sum_right /* (double_t) count_right*/ << std::endl;
+    std::cout << "=========================================" << std::endl;
+
+  }
+
 }
 
 string GNB::predict(vector<double>)
--- L3/naive_bayes/solution/classifier.h
+++ L3/naive_bayes/solution/classifier.h
@@ -9,6 +9,14 @@
 using namespace std;
 
 class GNB {
+  float mean_left;
+  float mean_right;
+  float mean_keep;
+
+  float var_left;
+  float var_right;
+  float var_keep;
+  const float lane_width = 4.0f;
 public:
 
   vector<string> possible_labels = {"left","keep","right"};
